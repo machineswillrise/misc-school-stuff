55df63a63b835b665686d0e36c66fa3b53ae4692
MachinesWillRise <machineswillrise@protonmail.com>
2026-07-18 17:08:15 -0400

    Replace custom toRadians with Math.toRadians in ImageRotator

--- ImageRotator.java
+++ ImageRotator.java
@@ -13,17 +13,13 @@ class ImageRotator {
 		this.originalImage = originalImage;
 	}
 
-	private double toRadians(int deg) {
-		return Math.PI * deg / 180.0;
-	}
-
 	public BufferedImage getRotatedImage(int deg) {
 		AffineTransform transform = new AffineTransform();
 
 		int halfWidth = originalImage.getWidth() / 2;
 		int halfHeight = originalImage.getHeight() / 2;
 
-		transform.rotate(toRadians(deg), halfWidth, halfHeight);
+		transform.rotate(Math.toRadians(deg), halfWidth, halfHeight);
 		// This is optional if you want to make it to spin around the center instead of rotating the whole image
 		// transform.translate(-halfWidth, -halfHeight);
 
